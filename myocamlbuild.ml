6e776d4a77ad3198d9e63ad0b92a825f9663a0ee
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2013-03-01 22:36:52 +0000

    Change the name libvhd -> libxcpvhd
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 85178d52e6eb215c8c3cf1950978a5a6) *)
+(* DO NOT EDIT (digest: 4c413e98106358da75e3669acc9cc100) *)
 module OASISGettext = struct
-(* # 21 "/local/scratch/djs/.opam/system/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
+(* # 21 "/home/jludlam/.opam/ea1254/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
 
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-(* # 21 "/local/scratch/djs/.opam/system/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
+(* # 21 "/home/jludlam/.opam/ea1254/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
 
 
 
@@ -116,7 +116,7 @@ end
 
 # 117 "myocamlbuild.ml"
 module BaseEnvLight = struct
-(* # 21 "/local/scratch/djs/.opam/system/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
+(* # 21 "/home/jludlam/.opam/ea1254/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
 
   module MapString = Map.Make(String)
 
@@ -214,7 +214,7 @@ end
 
 # 215 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
-(* # 21 "/local/scratch/djs/.opam/system/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
+(* # 21 "/home/jludlam/.opam/ea1254/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
 
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -323,7 +323,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-(* # 21 "/local/scratch/djs/.opam/system/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 21 "/home/jludlam/.opam/ea1254/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -339,7 +339,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
 
-(* # 56 "/local/scratch/djs/.opam/system/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 56 "/home/jludlam/.opam/ea1254/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   type t =
       {
@@ -477,16 +477,16 @@ end
 open Ocamlbuild_plugin;;
 let package_default =
   {
-     MyOCamlbuildBase.lib_ocaml = [("libvhd", ["lib"])];
-     lib_c = [("libvhd", "lib", [])];
+     MyOCamlbuildBase.lib_ocaml = [("libxcpvhd", ["lib"])];
+     lib_c = [("libxcpvhd", "lib", [])];
      flags =
        [
-          (["oasis_library_libvhd_cclib"; "link"],
+          (["oasis_library_libxcpvhd_cclib"; "link"],
             [
                (OASISExpr.EBool true,
                  S [A "-cclib"; A "-lvhd"; A "-cclib"; A "-lvhdio"])
             ]);
-          (["oasis_library_libvhd_cclib"; "ocamlmklib"; "c"],
+          (["oasis_library_libxcpvhd_cclib"; "ocamlmklib"; "c"],
             [(OASISExpr.EBool true, S [A "-lvhd"; A "-lvhdio"])])
        ];
      includes = [];
